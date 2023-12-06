1d817b5b736740818145339c6f171f68614af848
João Afonso <joaosilvaafonso14@gmail.com>
2023-12-05 20:58:32 -0400

    first commit

--- src/items/use-cases/create-item.use-case.ts
+++ src/items/use-cases/create-item.use-case.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { Item } from '../../shared/entities/Item.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { CreateItemDto } from '../dto/create-Item.dto';
+import { Item } from 'src/shared/entities/item.entity';
+import { CreateItemDto } from 'src/items/dto/create-item.dto';
 
 @Injectable()
 export class CreateItemUseCase {
